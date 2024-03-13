eaa00bb78551835c4fff779b341a049bb3419040
Nehir0109 <nehir.cakilci@gmail.com>
2024-03-14 01:33:24 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner get= new Scanner(System.in);
+
+        String[] students = {"Burak", "Ahmet", "Nazlı", "Zehra"};
+        String[] subjects = {"Matematik", "Fizik", "Kimya", "Biyoloji"};
+        int[][] grades= new int[students.length][subjects.length];
+
+        System.out.println("Puan Hesaplama Sistemine Hoşgeldiniz!\n");
+
+        for(int i=0; i< students.length; i++){
+            for(int j=0; j< subjects.length; j++){
+                System.out.print(students[i]+ " için "+ subjects[j]+ " notunu giriniz: ");
+                grades[i][j]= get.nextInt();
+            }
+        }
+
+        System.out.println("\nNot Ortalamaları: \n");
+
+        for(int i=0; i< students.length; i++){
+            int sum=0;
+            for(int j=0; j< subjects.length; j++){
+                sum+= grades[i][j];
+            }
+            double avarage = (double) sum/ subjects.length;
+            System.out.println(students[i]+ ": "+ avarage);
+        }
+
+        System.out.println("\nDers için Sınıf Ortalaması: \n");
+        for(int i=0; i< subjects.length; i++){
+            int sum =0;
+            for(int j=0; j< students.length; j++){
+                sum+= grades[j][i];
+            }
+            double avarage = (double) sum/ subjects.length;
+            System.out.println(students[i]+ ": "+ avarage);
+        }
+
+
+        int classSum= 0;
+        for(int i=0; i< students.length; i++){
+            for(int j=0; j< subjects.length; j++){
+                classSum += grades[i][j];
+            }
+        }
+        double classAverage = (double) classSum/ (students.length * subjects.length);
+        System.out.println("\nSınıf Ortalaması: "+ classAverage);
+    }
+}
